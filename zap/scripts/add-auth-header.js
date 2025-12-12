3068b0a8f0b1ce8168eeeeb9b976afaa49d51e5a
lookattheroadtest-tech <lookattheroadtest@gmail.com>
2025-12-12 13:58:56 +0530

    Update add-auth-header.js

--- zap/scripts/add-auth-header.js
+++ zap/scripts/add-auth-header.js
@@ -1,7 +1,7 @@
 // ZAP HTTP Sender Script - Forces exact Authorization header on every request to the target
 function sender(msg, initiator, helper) {
     var uri = msg.getRequestHeader().getURI();
-    if (uri.getHost().equals("demo.testfire.net") && uri.getPath().startsWith("/api/admin/addUser")) {
+    if (uri.getHost().equals("demo.testfire.net") && uri.getPath().equals("/api/admin/addUser")) {
         msg.getRequestHeader().setHeader("Authorization", "YW5OdGFYUm86WkdWdGJ6RXlNelE9Oj9lPz8/P2c=");
         msg.getRequestHeader().setHeader("Content-Type", "application/json");
         msg.getRequestHeader().setHeader("Accept", "application/json");
