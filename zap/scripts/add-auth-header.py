6dd35757eba6f49f7543b188c8d673bfe5d46dd0
lookattheroadtest-tech <lookattheroadtest@gmail.com>
2025-12-12 14:11:07 +0530

    Create add-auth-header.py

---
+++ zap/scripts/add-auth-header.py
@@ -0,0 +1,7 @@
+# zap/scripts/add-auth-header.py
+def zap_api_scan_hook(request, response, api_scan):
+    if "/api/admin/addUser" in request.url:
+        request.headers["Authorization"] = "YW5OdGFYUm86WkdWdGJ6RXlNelE9Oj9lPz8/P2c="
+        request.headers["Content-Type"] = "application/json"
+        request.headers["Accept"] = "application/json"
+    return request
